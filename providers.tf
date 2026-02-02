22b69b6408d25102bd2df2f01e09ed6e667eccc7
deepak771991-svg <deepak771991@gmail.com>
2026-02-02 17:48:25 +0530

    providers update

---
+++ providers.tf
@@ -0,0 +1,20 @@
+terraform {
+ backend "azurerm" {
+   resource_group_name  = "deepak-resource-group-one"
+   storage_account_name = "myterraformbackendsa"
+   container_name       = "tfstate"
+   key                  = "terraform.tfstate"
+ }
+}
+
+
+
+
+provider "azurerm" {
+   features {}
+ }
+
+resource "azurerm_resource_group" "one" {
+   name     = "deepak-resource-githubaction"
+   location = "West US"
+}
