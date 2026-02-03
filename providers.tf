0d5ca2fe7a3a144ccd5f76a5947f67bf60d8f23a
deepak771991-svg <deepak771991@gmail.com>
2026-02-03 14:46:39 +0530

    update

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 terraform {
  backend "azurerm" {
    resource_group_name  = "deepak-resource-group-one"
-   storage_account_name = "myterraformbackendsa"
+   storage_account_name = "deepakstoragenewaccount1"
    container_name       = "tfstate"
    key                  = "terraform.tfstate"
  }
